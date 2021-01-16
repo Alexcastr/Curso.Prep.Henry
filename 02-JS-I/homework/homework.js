b30c42d437f4579c6b6cda48b37c3a17bef4a3a6
Alex Castro <castro.t.alex@gmail.com>
2021-01-16 11:22:09 -0500

    activity

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,17 +28,26 @@ const nuevoModulo = 21 % 5 === 1;
 function devolverString(str) {
     // "Return" la string provista: str
     // Tu código:
+
+    return str;
 }
 
 function suma(x, y) {
     // "x" e "y" son números
     // Suma "x" e "y" juntos y devuelve el valor
     // Tu código:
+    return x + y;
+
+    suma(5, 5)
 }
 
 function resta(x, y) {
     // Resta "y" de "x" y devuelve el valor
     // Tu código:
+    return x - y;
+
+    resta(5, 5)
+
 }
 
 function multiplica(x, y) {
