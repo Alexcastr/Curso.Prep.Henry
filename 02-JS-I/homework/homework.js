954bb12b3082da44e55c8dc2693c49ec3c10e373
Alex Castro <castro.t.alex@gmail.com>
2021-01-20 10:02:08 -0500

    activities

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -70,6 +70,12 @@ function sonIguales(x, y) {
     // Devuelve "true" si "x" e "y" son iguales
     // De lo contrario, devuelve "false"
     // Tu código:
+    if (x == y) {
+        return true;
+    } else {
+        return false;
+    }
+
 
 }
 
@@ -77,6 +83,12 @@ function tienenMismaLongitud(str1, str2) {
     // Devuelve "true" si las dos strings tienen la misma longitud
     // De lo contrario, devuelve "false"
     // Tu código:
+
+    if (str1.length === str2.length) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function menosQueNoventa(num) {
