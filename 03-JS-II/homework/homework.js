c351e176e57285dff54976398b4c4a17d2c89983
Alex Castro <castro.t.alex@gmail.com>
2021-01-27 10:15:20 -0500

    FinishSecondOne

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -1,4 +1,5 @@
 // No cambies los nombres de las funciones.
+//https://github.com/atralice/Curso.Prep.Henry/blob/solution/03-JS-II/homework/homework.js
 
 function obtenerMayor(x, y) {
     // "x" e "y" son números enteros (int).
@@ -31,12 +32,9 @@ function conection(status) {
 
     if (status === 1) {
         return "online";
-    }
-    if
-    else(status === 2) {
+    } else if (status === 2) {
         return "away";
-    }
-    else {
+    } else {
         return "offline";
     }
 }
@@ -111,69 +109,123 @@ function colors(color) {
         // Si "numero" es divisible entre 5, devuelve "buzz"
         // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
         // De lo contrario, devuelve el numero
+        if (numero % 15 === 0) return 'fizzBuzz';
+
+        if (numero % 5 === 0)
+            return 'buzz';
+
+
+        if (numero % 3 === 0)
+            return 'fiz';
+
+        return numero;
+    }
+}
+
+function operadoresLogicos(num1, num2, num3) {
+    //La función recibe tres números distintos. 
+    //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
+    //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
+    //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
+    //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
+    //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+
+    if (num1 > 0 && num1 > num2 && num1 > num3) {
+        return "Número 1 es mayor y positivo";
+    } else if (num1 < 0 || num2 < 0 || num3 < 0) {
+        return "Hay negativos";
+    } else if (num3 > num1 && num3 > num2) {
+        return num3 + 1;
+    } else if (num1 === 0 || num2 === 0 || num3 === 0) {
+        return 'error';
+    } else {
+        return false;
     }
 
-    function operadoresLogicos(num1, num2, num3) {
-        //La función recibe tres números distintos. 
-        //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
-        //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
-        //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
-        //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
-        //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+}
+
+function esPrimo(numero) {
+    // Devuelve "true" si "numero" es primo
+    // De lo contrario devuelve "falso"
+    // Pista: un número primo solo es divisible por sí mismo y por 1
+    // Pista 2: Puedes resolverlo usando un bucle `for`
+    // Nota: Los números 0 y 1 NO son considerados números primos
+    if (numero < 2) return false;
+    if (numero === 2) return true;
+    for (var i = 2; i < numero; i++) {
+        if (numero % i === 0) {
+            return false;
+        }
     }
+    return true;
 
-    function esPrimo(numero) {
-        // Devuelve "true" si "numero" es primo
-        // De lo contrario devuelve "falso"
-        // Pista: un número primo solo es divisible por sí mismo y por 1
-        // Pista 2: Puedes resolverlo usando un bucle `for`
-        // Nota: Los números 0 y 1 NO son considerados números primos
+
+}
+
+function esVerdadero(valor) {
+    //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
+    //si su valor es true y “Soy falso” si su valor es false.
+    //Escribe tu código aquí
+    if (valor === true) {
+        return "Soy verdadero"
     }
+    return "Soy falso"
 
-    function esVerdadero(valor) {
-        //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
-        //si su valor es true y “Soy falso” si su valor es false.
-        //Escribe tu código aquí
+}
 
+function tablaDelSeis() {
+    //Escribe una función que muestre la tabla de multiplicar del 6 (del 6 al 60).
+    //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
+    //Escribe tu código aquí   
+    let arrayTablaDel6 = []
+    for (let i = 0; i < 11; i++) {
+        arrayTablaDel6.push(6 * i)
     }
+    return arrayTablaDel6
+}
 
-    function tablaDelSeis() {
-        //Escribe una función que muestre la tabla de multiplicar del 6 (del 6 al 60).
-        //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-        //Escribe tu código aquí   
-
+function tieneTresDigitos(numero) {
+    //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
+    //Escribe tu código aquí
+    if (numero > 99 && numero < 1000) {
+        return true
     }
+    return false
+}
 
-    function tieneTresDigitos(numero) {
-        //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
-        //Escribe tu código aquí
+function doWhile(numero) {
+    //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
+    //Retornar el valor final.
+    //Usar el bucle do ... while.
 
+    var a = numero;
+    var i = 0;
+    do {
+        i = i + 1;
+        a = a + 5;
     }
-
-    function doWhile(numero) {
-        //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
-        //Retornar el valor final.
-        //Usar el bucle do ... while.
-    }
+    while (i < 8);
+    return a;
+}
 
 
-    // No modificar nada debajo de esta línea
-    // --------------------------------
+// No modificar nada debajo de esta línea
+// --------------------------------
 
-    module.exports = {
-        obtenerMayor,
-        mayoriaDeEdad,
-        conection,
-        saludo,
-        colors,
-        esDiezOCinco,
-        estaEnRango,
-        esEntero,
-        fizzBuzz,
-        operadoresLogicos,
-        esPrimo,
-        esVerdadero,
-        tablaDelSeis,
-        tieneTresDigitos,
-        doWhile
-    };
+module.exports = {
+    obtenerMayor,
+    mayoriaDeEdad,
+    conection,
+    saludo,
+    colors,
+    esDiezOCinco,
+    estaEnRango,
+    esEntero,
+    fizzBuzz,
+    operadoresLogicos,
+    esPrimo,
+    esVerdadero,
+    tablaDelSeis,
+    tieneTresDigitos,
+    doWhile
+};
